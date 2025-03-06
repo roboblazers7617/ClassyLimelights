c854030bbe3bcd66afa3541b3644ba290d030384
Apollo Nargang <CoffeeCoder1@outlook.com>
2025-03-06 11:10:26 -0500

    Add package info files to more packages

---
+++ src/main/java/io/github/roboblazers7617/limelight/targets/neural/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes that represent targets detected by the Limelight's neural pipelines.
+ */
+package io.github.roboblazers7617.limelight.targets.neural;
---
+++ src/main/java/io/github/roboblazers7617/limelight/targets/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes that represent targets detected by the Limelight.
+ */
+package io.github.roboblazers7617.limelight.targets;
